e12d815712e948c11694738a2eaf87c60de572de
Nethum1040 <nethumoshada104@gmail.com>
2025-09-05 22:18:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;acFAFLSQ#bMGQ9TPenxU28awIG1AJ92svG884wyUxbeJWuebfYWE", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
